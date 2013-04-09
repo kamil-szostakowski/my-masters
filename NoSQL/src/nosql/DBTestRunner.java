d1fa226328bc57f82ef2ace5eda1d357d41b316d
Kamil Szostakowski <kamil_666@wp.pl>
2013-04-09 21:23:25 +0200

    Dodanie brakującego pliku

--- NoSQL/src/nosql/DBTestRunner.java
+++ NoSQL/src/nosql/DBTestRunner.java
@@ -15,39 +15,43 @@ public class DBTestRunner
     private IDatabaseJob job;
     private Thread[] threads;
     
+    /*
+     * Metoda pozwala na zdefiniowanie przebiegu dla zadania.
+     */
+    
     public void SetTest(IDatabaseRun test)
     {
         this.test = test;
     }
+    
+    /*
+     * Metoda pozwala na zdefiniowanie zadania do wykonania w bazie danych.
+     */
     
     public void SetJob(IDatabaseJob job)
     {
         this.job = job;
     }
     
+    /*
+     * Metoda uruchamia zadanie w bazie.
+     */
+    
     public void RunTest()
     {        
-        try
-        {
-            this.threads = new Thread[test.GetNumberOfThreads()];
+        this.threads = new Thread[test.GetNumberOfThreads()];
         
-            for(int iter=0; iter<test.GetNumberOfThreads(); iter++)
-            {                                                    
-                this.threads[iter] = new Thread((Runnable)this.job);
+        for(int iter=0; iter<test.GetNumberOfThreads(); iter++)
+        {                                                    
+            this.threads[iter] = new Thread((Runnable)this.job);
             
-                this.job.SetThreadID(iter);            
-                this.job.SetLogFile(String.format("%s-thread-%d.log", this.job.GetName(), iter));
-                this.job.SetConfiguration(test);
+            this.job.SetThreadID(iter);            
+            this.job.SetLogFile(String.format("%s-thread-%d.log", this.job.GetName(), iter));
+            this.job.SetConfiguration(test);
                 
-                this.threads[iter].start();  
+            this.threads[iter].start();  
             
-                this.job = this.job.Clone();
-            }
-        }
-        
-        catch (Exception ex)
-        {
-              Logger.getLogger(DBTestRunner.class.getName()).log(Level.SEVERE, null, ex);      
-        }
+            this.job = this.job.Clone();
+        }             
     }
 }
